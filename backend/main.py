6c8d172a4b42c3120f1256f8883e47c513cb0547
unknown <guilherme.antunes2223@gmail.com>
2026-02-03 18:05:50 -0500

    Adicionando rota principal e ajuste de URL

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,18 @@ from backend.routes import router
 from fastapi.middleware.cors import CORSMiddleware
 import os
 import uvicorn
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+frontend_path = os.path.join(os.getcwd(), "frontend")
+
+@app.get("/")
+async def read_index():
+    return FileResponse(os.path.join(frontend_path, "index.html"))
 
 app.add_middleware(
     CORSMiddleware, 
